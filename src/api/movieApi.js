3162e03f1b1cdb1807c0a790e2499ddfff5fc6d4
Hiten Vadhaiya <130130815+1994Chang@users.noreply.github.com>
2024-05-30 17:20:55 +0530

    init

--- src/api/movieApi.js
+++ src/api/movieApi.js
@@ -2,11 +2,12 @@ import axios from "axios";
 
 
 const apiKey = process.env.REACT_APP_API_KEY;
-console.log(apiKey,"apikey")
+const apiUrl = process.env.REACT_APP_API_URL;
+// console.log(apiKey,"apikey")
  export  const getMovielist = async(movieName) =>{
         try {
 
-            const res = await axios(`https://www.omdbapi.com/?s=${movieName}&apikey=${apiKey}&page=1`);
+            const res = await axios(`${apiUrl}/?s=${movieName}&apikey=${apiKey}&page=1`);
             console.log(res,"resorce in api")
             return res;
         } catch (error) {
